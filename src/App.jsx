bdd959113ff5c51875a5c3993dfa7b610f405c19
nikil-20 <nikilgajankush@gmail.com>
2025-06-07 14:38:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {Header,Header2,Header3} from './Components/Header'
+import './App.css'
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+    <div>hello world</div>
+    <div> <Header/>  </div>
+    <div> <Header2/>  </div>
+    <div>  <Header3/> </div>
+    
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+function Header() {
+
+  return (
+    <>
+    <div>this is my header from web app</div>
+
+    </>
+  )
+}
+
+function Header2(){
+    return (
+        <>
+        <div>this is my header 2</div>
+        </>
+    )
+}
+
+function Header3(){
+    return (
+        <>
+        <div>this is my header 3</div>
+        </>
+    )
+}
+
+export  {Header,Header2, Header3}
+
