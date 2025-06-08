345dcae317c583f3c94eae989886d8726e0a55d1
nikil-20 <nikilgajankush@gmail.com>
2025-06-08 17:40:25 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Conditional from './Components/Conditional'
 import APICall from './Components/APICall'
 import { Header } from './Components/Header'
+import Home from './Components/Home'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 
@@ -18,20 +19,25 @@ function App() {
     <Conditional/> */}
 
       {/* <APICall/> */}
-{/* 
-      <Router>
-        <link to="/header"  >Header</link>
-        <link to="/apiCall"  >API Call</link>
+
+
+      <Router >
+        <Link to="/" >Home</Link>
+        <Link to="/header"  >Header</Link>
+        <Link to="/apiCall"  >API Call</Link>
+        <Link to="/conditional"  >Conditional</Link>
+        <Link to="/section"  > Section </Link>
 
         <Routes>
+          <Route index  element={<Home/>}  ></Route>
           <Route path='header' element={<Header />} ></Route>
           <Route path='apiCall' element={<APICall />} ></Route>
-
+          <Route path='conditional' element={<Conditional/>}  ></Route>
+          <Route path='section' element={<Section/>}  ></Route>
         </Routes>
 
-      </Router> */}
+      </Router>
 
-      <div> <APICall/> </div>
 
     </>
   )
